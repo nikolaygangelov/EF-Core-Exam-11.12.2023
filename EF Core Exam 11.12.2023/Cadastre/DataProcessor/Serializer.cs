2b827b1205dd186f5d8adb8b62c3609b6cd080af
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2024-01-03 23:25:19 +0200

    Update Serializer.cs

--- EF Core Exam 11.12.2023/Cadastre/DataProcessor/Serializer.cs
+++ EF Core Exam 11.12.2023/Cadastre/DataProcessor/Serializer.cs
@@ -13,8 +13,11 @@ namespace Cadastre.DataProcessor
     {
         public static string ExportPropertiesWithOwners(CadastreContext dbContext)
         {
-            DateTime givenDate = DateTime.ParseExact("01/01/2000", "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            //date which has to compare to
+            DateTime givenDate = DateTime.ParseExact("01/01/2000", "dd/MM/yyyy", CultureInfo.InvariantCulture); //using culture-independent format
 
+            //turning needed info about properties into a collection using anonymous object
+            //using less data
             var propertiesAndOwners = dbContext.Properties
                  .Where(p => p.DateOfAcquisition >= givenDate)
                  .OrderByDescending(p => p.DateOfAcquisition)
@@ -36,18 +39,23 @@ namespace Cadastre.DataProcessor
                  })
                  .ToArray();
 
-
+            //Serialize method needs object to convert/map
+	        //adding Formatting for better reading 
             return JsonConvert.SerializeObject(propertiesAndOwners, Newtonsoft.Json.Formatting.Indented);
         }
 
         public static string ExportFilteredPropertiesWithDistrict(CadastreContext dbContext)
         {
+            //using Data Transfer Object Class to map it with properties
             XmlSerializer serializer = new XmlSerializer(typeof(ExportPropertiesDTO[]), new XmlRootAttribute("Properties"));
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
 
+            //"using" automatically closes opened connections
             using var writer = new StringWriter(sb);
 
+            //setting tabular identation
             XmlWriterSettings settings = new XmlWriterSettings
             {
                 Indent = true,
@@ -57,6 +65,8 @@ namespace Cadastre.DataProcessor
             using var xmlwriter = XmlWriter.Create(writer, settings);
 
             var xns = new XmlSerializerNamespaces();
+
+            //one way to display empty namespace in resulted file
             xns.Add(string.Empty, string.Empty);
 
             var propertiesAndDistricts = dbContext.Properties
@@ -65,6 +75,7 @@ namespace Cadastre.DataProcessor
                 .ThenBy(p => p.DateOfAcquisition)
                 .Select(p => new ExportPropertiesDTO
                 {
+                    //using identical properties in order to map successfully
                     PostalCode = p.District.PostalCode,
                     PropertyIdentifier = p.PropertyIdentifier,
                     Area = (long)p.Area,
@@ -72,10 +83,14 @@ namespace Cadastre.DataProcessor
                 })
                 .ToArray();
 
+            //Serialize method needs file, TextReader object and namespace to convert/map
             serializer.Serialize(xmlwriter, propertiesAndDistricts, xns);
+
+            //explicitly closing connection in terms of reaching edge cases
             writer.Close();
 
-            return sb.ToString();
+            //using TrimEnd() to get rid of white spaces
+            return sb.ToString().TrimEnd();
         }
     }
 }
